2acd1bc72adde961994c3999743e9c4b9120736c
Reza <reza55n@gmail.com>
2023-12-30 00:14:56 +0330

    Create storage1.ts

---
+++ storage1.ts
@@ -0,0 +1,150 @@
+import { create } from "zustand"
+import { persist, PersistStorage } from "zustand/middleware"
+import { shallow } from "zustand/shallow"
+import { BroadcastChannel, createLeaderElection } from 'broadcast-channel'
+
+
+// #############################################################################
+
+const valInit = 0
+
+type valType = number
+// Can be primitive or any mixtures of array/object/primitive
+// (You can set `any`)
+
+const name = "storage1"
+// Must be unique in the project. Will be named in localStorage and ...
+// ...sessionStorage with some prefixes
+
+// Also you can find and customize/add/remove the methods `increase`, ...
+// ...`decrease` and `reset`. !! IMPORTANT: Don't forget to set ...
+// ...`doPost` to `true` for the methods (except for setVal).
+
+const verbose = false
+// true: Logs are displayed
+
+const secure = true
+// In modes other than native, sends an empty message shortly after it's ...
+// ...been received
+
+// #############################################################################
+
+
+const log = (msg) => verbose &&
+  console.log(`${msg}              ${Date.now() - Math.floor(Date.now() / 10000) * 10000}`)
+
+const debounce = (func, timeout = 300) => {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => { func.apply(this, args) }, timeout)
+  }
+}
+
+
+// Channel initialization
+
+var channel = new BroadcastChannel(name)
+if (channel.type === "idb") // It's not secure nor efficient
+  channel = new BroadcastChannel(name, {type: "localstorage"})
+log (`Broadcast channel '${name}' initialized with type of ${channel.type}`)
+
+const clearMessageDebounced = debounce(() => channel.type !== "native" && secure &&
+  channel.postMessage(""), 1000)
+
+const elector = createLeaderElection(channel)
+var imLeader = await elector.hasLeader()
+imLeader = !imLeader
+var waitingForState
+if (imLeader) {
+  log("I'm the leader")
+  waitingForState = false
+} else {
+  log('Another tab and leader exists')
+  channel.postMessage("ask_leader_for_state")
+  waitingForState = true
+}
+elector.awaitLeadership().then(() => {
+  log("I'm the leader now")
+  imLeader = true
+})
+
+channel.onmessage = async msg => {
+  if (verbose)
+    console.log(`### NEW MESSAGE, Am I leader? ` + imLeader + ", message: ", msg)
+  
+  if (msg) { // To prevent infinite loop
+    if (msg === "ask_leader_for_state") {
+      if (imLeader) {
+        log("Re-sending the values for the new tab.......")
+        var states = useStore.getState()
+        states.setVal(states.val)
+      }
+      return
+    }
+    
+    if (msg?.num !== undefined) {
+      var msgJ = JSON.parse(msg.num)
+      var valll = msgJ?.state?.val
+      if (valll !== undefined) {
+        waitingForState = false
+        useStore.getState().setVal(valll, false)
+      }
+    }
+    clearMessageDebounced()
+  }
+}
+
+
+interface BearState {
+  val: valType
+  doPost: boolean
+  setVal: (val: valType, doPost?: boolean) => void
+  increase: () => void
+  decrease: () => void
+  reset: () => void
+}
+
+const storage: PersistStorage<BearState> = {
+  getItem: (key) => {
+    const str = sessionStorage[key]
+    if (!str) return null
+    return JSON.parse(str)
+  },
+  setItem: (key, newValue) => {
+    if (waitingForState)
+      return
+    
+    const newValueStr = JSON.stringify(newValue)
+    sessionStorage[key] = newValueStr
+    if (newValue.state.doPost === true) {
+      log("Setter not from broadcast. Posting.......")
+      channel.postMessage({num: newValueStr})
+    } else
+      log("Setter from broadcast, not posting.")
+  },
+  removeItem: (key): void => {
+    delete sessionStorage[key]
+  }
+}
+
+export const useStore = create<BearState>()(
+  persist(
+    (set, get) => ({
+      val: valInit,
+      doPost: true,
+      setVal: (val, doPost = true) => set({ val: val, doPost }),
+      increase: () => set((state) => ({ val: state.val + 1, doPost: true })),
+      decrease: () => set((state) => ({ val: state.val - 1, doPost: true })),
+      reset: () => set({ val: valInit, doPost: true }),
+    }),
+    { name: `zustand-${name}`, storage }
+  )
+)
+
+export const useStoreSet = () => useStore(
+  (state) => [state.setVal, state.increase, state.decrease, state.reset],
+  shallow
+)
+
+export const useStoreGet = () => useStore((state) => state.val)
