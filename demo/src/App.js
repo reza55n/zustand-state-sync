dfa05bb2d78e8f00b3099cf8fca4c2e05adb47bd
Reza Nooralizadeh <reza55n@gmail.com>
2023-12-31 13:30:51 +0330

    UI minor fix

--- demo/src/App.js
+++ demo/src/App.js
@@ -53,52 +53,55 @@ function App() {
   useLogAdd("App rendered")
   return (
     <>
-      <div className="App">
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "8px"}}>
-          <a className="button" target="_blank" href="https://github.com/reza55n/zustand-state-sync">GitHub</a>
-          <button style={{fontSize: "82%"}} className="button" onClick={() => {
-            window.open(".", "_blank")
-          }}>+ New tab</button>
+      <h1>Zustand State Sync - Demo</h1>
+      <div id="container">
+        <div className="App">
+          <div style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "8px"}}>
+            <a className="button" target="_blank" href="https://github.com/reza55n/zustand-state-sync">GitHub</a>
+            <button style={{fontSize: "82%"}} className="button" onClick={() => {
+              window.open(".", "_blank")
+            }}>+ New tab</button>
+          </div>
+          <hr/>
+          <h2>storage1 (number)</h2>
+          <button style={{fontSize: "80%"}} onClick={async () => {
+            for (var i = 0; i < 100; i++) {
+              increase()
+            }
+          }}>+= 100 separate posts</button><br/>
+          <div style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "8px"}}>
+            <button onClick={decrease}>-</button>{" "}
+            <Store1Get/>
+            <button onClick={increase}>+</button><br/>
+          </div>
+          <button style={{fontSize: "80%"}} onClick={async () => {
+            for (var i = 0; i < 100; i++) {
+              decrease()
+            }
+          }}>-= 100 separate posts</button><br/>
+          <button style={{fontSize: "92%"}} onClick={() => {
+              var val = parseInt(prompt("Enter value"))
+              if (!isNaN(val))
+                setVal((prev) => prev + val)
+            }}>+= custom value</button>
+          <hr/>
+          <h2>storage2 (array of string)</h2>
+          <p style={{fontSize: "85%"}}>Phrases from <a target="_blank" href="https://randomwordgenerator.com/phrase.php">here</a></p>
+          <Store2Set/>
+          <Store2Get/>
+          <h3>From another file:</h3>
+          <Comp/>
         </div>
-        <hr/>
-        <h2>storage1 (number)</h2>
-        <button style={{fontSize: "80%"}} onClick={async () => {
-          for (var i = 0; i < 100; i++) {
-            increase()
-          }
-        }}>+= 100 separate posts</button><br/>
-        <div style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "8px"}}>
-          <button onClick={decrease}>-</button>{" "}
-          <Store1Get/>
-          <button onClick={increase}>+</button><br/>
+        <div id="log">
+          <h2>
+            <span>log </span>
+            <button onClick={() => {
+                resetLog()
+              }}>Clear</button>
+          </h2>
+          <p style={{fontSize: "85%", color: "green", fontWeight: "bold"}}>(The log is stored separately for each tab)</p>
+          <LogBody/>
         </div>
-        <button style={{fontSize: "80%"}} onClick={async () => {
-          for (var i = 0; i < 100; i++) {
-            decrease()
-          }
-        }}>-=100 separate posts</button><br/>
-        <button style={{fontSize: "92%"}} onClick={() => {
-            var val = parseInt(prompt("Enter value"))
-            if (!isNaN(val))
-              setVal((prev) => prev + val)
-          }}>+= custom value</button>
-        <hr/>
-        <h2>storage2 (array of string)</h2>
-        <p style={{fontSize: "85%"}}>Phrases from <a target="_blank" href="https://randomwordgenerator.com/phrase.php">here</a></p>
-        <Store2Set/>
-        <Store2Get/>
-        <h3>From another file:</h3>
-        <Comp/>
-      </div>
-      <div id="log">
-        <h2>
-          <span>log </span>
-          <button onClick={() => {
-              resetLog()
-            }}>Clear</button>
-        </h2>
-        <p style={{fontSize: "85%", color: "green", fontWeight: "bold"}}>(The log is stored separately for each tab)</p>
-        <LogBody/>
       </div>
     </>
   );
